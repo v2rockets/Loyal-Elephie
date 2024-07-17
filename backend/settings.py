d4587da5e58702630628661df05747222c8bc9cb
v2rockets <zypcastle@hotmail.com>
2024-07-17 00:43:20 -0400

    Update prompt

--- backend/settings.py
+++ backend/settings.py
@@ -38,7 +38,7 @@ BM25_WEIGHT = 0.1  # Weight given to the BM25 score when adjusting the final sco
 
 # ---Prompts--- #
 SUMMARY_PROMPT='''You are the "ASSISTANT" and your task is to take a detailed note about {NICK_NAME} from a conversation with you. You should focus on observations on {NICK_NAME}'s situation and special things mentioned by him but you doesn't need to include assistant's (your own) words unless addressed by {NICK_NAME}.{LANGUAGE_PREFERENCE} Don't write a title and don't write anything else before or after the note.'''
-SUMMARY_NOTE_PROMPT='''Your task is to write a comprehensive summary about the Note provided by the user {NICK_NAME}. The summary should be written as a bullet list of self-contained items without a title.{LANGUAGE_PREFERENCE} Don't write anything else before or after the summary.'''
+SUMMARY_NOTE_PROMPT='''Your task is to write a comprehensive summary about the Note authored by the user mentioned as *{NICK_NAME}*. The summary should be written as a bullet list of self-contained items without a title.{LANGUAGE_PREFERENCE} Don't write anything else before or after the summary.'''
 
 AGENT_PROMPT = '''You are Loyal Elephie, {NICK_NAME}'s autonomous secretary who has access to the following tools:
 1. You have an inner monologue section which could help you analyze the problem without disturbing {NICK_NAME}. To use inner monologue section, write your monologue between tags "<THINK>" and "</THINK>". The monologue should including the user problem breakdown the questions you don't yet understand. This tool is how you comprehend.
